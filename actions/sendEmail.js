0965a69be9b361eb8ad3828f029665f90a5b5f4e
unknown <youssef.ojeil@hotmail.com>
2024-08-18 16:43:46 -0400

    create dockerfile and cloudbuild file cloud run and artifact registry setup

--- actions/sendEmail.js
+++ actions/sendEmail.js
@@ -1,47 +1,47 @@
-"use server";
+// "use server";
 
-import { Resend } from "resend";
-import { validateString, getErrorMessage } from "@/lib/errors";
-import ContactFormEmail from "@/email/contact-form-email";
-import React from "react";
+// import { Resend } from "resend";
+// import { validateString, getErrorMessage } from "@/lib/errors";
+// import ContactFormEmail from "@/email/contact-form-email";
+// import React from "react";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (formData) => {
-  const senderEmail = formData.senderEmail;
-  const message = formData.message;
+// export const sendEmail = async (formData) => {
+//   const senderEmail = formData.senderEmail;
+//   const message = formData.message;
 
-  if (!validateString(senderEmail, 500)) {
-    return {
-      error: "Invalid sender email",
-    };
-  }
+//   if (!validateString(senderEmail, 500)) {
+//     return {
+//       error: "Invalid sender email",
+//     };
+//   }
 
-  if (!validateString(message, 5000)) {
-    return {
-      error: "Invalid message",
-    };
-  }
+//   if (!validateString(message, 5000)) {
+//     return {
+//       error: "Invalid message",
+//     };
+//   }
 
-  let data;
-  try {
-    data = await resend.emails.send({
-      from: "TekMout Contact Form <onboarding@resend.dev>",
-      to: "youssef.ojeil@gmail.com",
-      subject: "Message from contact form",
-      reply_to: senderEmail,
-      react: React.createElement(ContactFormEmail, {
-        message: message,
-        senderEmail: senderEmail,
-      }),
-    });
-  } catch (error) {
-    return {
-      error: getErrorMessage(error),
-    };
-  }
+//   let data;
+//   try {
+//     data = await resend.emails.send({
+//       from: "TekMout Contact Form <onboarding@resend.dev>",
+//       to: "youssef.ojeil@gmail.com",
+//       subject: "Message from contact form",
+//       reply_to: senderEmail,
+//       react: React.createElement(ContactFormEmail, {
+//         message: message,
+//         senderEmail: senderEmail,
+//       }),
+//     });
+//   } catch (error) {
+//     return {
+//       error: getErrorMessage(error),
+//     };
+//   }
 
-  return {
-    data,
-  };
-};
+//   return {
+//     data,
+//   };
+// };
